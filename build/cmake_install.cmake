2c4c93e4d8002f05c0d9f52a41c6a17ded4eb0a5
노솔빈 <nsbsb79@3ssoft.co.kr>
2024-07-24 09:11:57 +0900

    Delete trash files

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/nsbsb9910/Workspace/SDL3_Toolbar
+# Install script for directory: /Users/nsbsb9910/Desktop/SDL3_Workspace/SDL3_Toolbar
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/nsbsb9910/Workspace/SDL3_Toolbar/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/nsbsb9910/Desktop/SDL3_Workspace/SDL3_Toolbar/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
